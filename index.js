e9b67979a310edc68d45fbafc1d16fe3176e93d7
yash desai <yhdesai99@ymail.com>
2019-08-24 15:32:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 
 function getFirstSelector(name) {
-return document.getFirstSelector(name)
+return document.querySelector(name)
 }
 
-function functionName() {
-
+function nestedTarget() {
+return document.querySelector("#nested .target")
 }
+
+
+
+function increaseRankBy(n) {
+  const a = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+   for (let i = 0; i < a.length; i++) {
+    a[i].innerHTML = parseInt(a[i].innerHTML) + n;
+}
+
+ }
+
+
+ function deepestChild() {
+  const picard = document.getElementById('grand-node').querySelectorAll('div');
+    var last = picard.length -1;
+        return picard[last];
+
+ } 
