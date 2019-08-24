c5428496bda3089da94bd4c8b630edec031931a5
yash desai <yhdesai99@ymail.com>
2019-08-24 15:32:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 
+function getFirstSelector(name) {
+return document.querySelector(name)
+}
+
+function nestedTarget() {
+return document.querySelector("#nested .target")
+}
+
+
+
+function increaseRankBy(n) {
+  const a = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+   for (let i = 0; i < a.length; i++) {
+    a[i].innerHTML = parseInt(a[i].innerHTML) + n;
+}
+
+ }
+
+
+ function deepestChild() {
+  var v = document.getElementById('grand-node').querySelectorAll('div');
+  return v[ v.length -1];
+
+ }
